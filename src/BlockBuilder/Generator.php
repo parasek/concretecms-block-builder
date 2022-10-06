fdbc9552eeccccbe031dd929c51bd297b125351c
parasek <slawek@radomski.net.pl>
2022-10-06 13:26:21 +0200

    Remove unnecessary item from json config

--- src/BlockBuilder/Generator.php
+++ src/BlockBuilder/Generator.php
@@ -354,6 +354,8 @@ class Generator
 
         $filename = 'config-bb.json';
 
+        unset($postData['refresh_block']);
+
         $fileService = new FileService();
         $fileService->append($postDataSummary['blockPath'] . DIRECTORY_SEPARATOR . $filename, json_encode($postData));
 
