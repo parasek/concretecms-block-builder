e474674ac6c37cedc99ebab33aaa907bbaeabd98
parasek <slawek@radomski.net.pl>
2025-09-02 15:31:43 +0200

    Refactor package controller

--- controller.php
+++ controller.php
@@ -4,39 +4,47 @@ namespace Concrete\Package\BlockBuilder;
 
 use Concrete\Core\Package\Package;
 use Concrete\Core\Page\Single as SinglePage;
+use Illuminate\Contracts\Container\BindingResolutionException;
 
 defined('C5_EXECUTE') or exit('Access Denied.');
 
 class Controller extends Package
 {
-    protected $pkgHandle = 'block_builder';
+    protected string $pkgHandle = 'block_builder';
     protected $appVersionRequired = '9.4.3';
-    protected $pkgVersion = '2.8.0';
+    protected string $pkgVersion = '2.8.0';
 
     protected $pkgAutoloaderRegistries = [
         'src/BlockBuilder' => 'BlockBuilder',
     ];
 
-    public function getPackageName()
+    public function getPackageName(): string
     {
         return t('Block Builder');
     }
 
-    public function getPackageDescription()
+    public function getPackageDescription(): string
     {
         return t('Build your custom Concrete CMS blocks (with optional set of repeatable entries).');
     }
 
-    public function on_start()
+    /**
+     * @throws BindingResolutionException
+     */
+    public function on_start(): void
     {
         $this->app->make('Concrete\Core\Routing\RouterInterface')->register('ajax/delete-block-type-folder', 'Concrete\Package\BlockBuilder\Controller\Ajax::deleteBlockTypeFolder');
     }
 
-    public function install()
+    public function install(): void
     {
         $pkg = parent::install();
 
-        // Install single pages
+        $this->installSinglePages($pkg);
+    }
+
+    private function installSinglePages($pkg): void
+    {
         $page = SinglePage::add('/dashboard/blocks/block_builder', $pkg);
         $page->updateCollectionName(t('Block Builder'));
     }
